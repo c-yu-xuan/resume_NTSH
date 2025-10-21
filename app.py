f73f8009b7008fac5cbd7182f5357eaf2cc8dd5a
c-yu-xuan <ariel103185@gmail.com>
2025-10-21 12:01:30 +0800

    Change index route to render new template

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('新北高中張育瑄')
 
 @app.route('/competition')
 def competition():
